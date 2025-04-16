98a14dfa1e76a70d4b08d81bcecf6e461883c5af
sinberowa <124888430+sinberowa@users.noreply.github.com>
2025-04-16 15:51:00 +0900

    20250416 ch06 start

---
+++ test6/Assets/scripts/catcontroller.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catcontroller : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    public float jumpforce = 680f;
+    public float walkforce = 30f;
+    public float maxwalkspeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        { 
+            rigid2D.AddForce(transform.up *  jumpforce);
+        }
+
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        float speedx = Mathf.Abs(rigid2D.velocity.x);
+
+        if(speedx < maxwalkspeed)
+        {
+            rigid2D.AddForce(transform.right * key * walkforce);
+        }
+
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+    }
+}
